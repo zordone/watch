b4e43760323db17c8d56601d78ebe50413f47536
Zord <zord.bk@gmail.com>
2018-08-13 19:30:48 +0200

    Added material ui table

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
 /* global fetch */
 
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ItemTable from './ItemTable';
 import './App.css';
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark'
+    }
+});
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            items: []
         };
     }
 
     componentDidMount() {
         fetch('http://localhost:3001/items')
             .then(res => res.json())
-            .then(list => list.map(item => ({
+            .then(items => items.map(item => ({
                 ...item,
                 id: item._id // eslint-disable-line no-underscore-dangle
             })))
-            .then(list => {
-                this.setState({ list });
+            .then(items => {
+                this.setState({ items });
             });
     }
 
     render() {
-        const { list } = this.state;
+        const { items } = this.state;
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">
-                        Watch
-                    </h1>
-                    <h2 className="App-subtitle">
-                        Movies and TV Shows
-                    </h2>
-                </header>
-                <ul>
-                    {list.map(item => (
-                        <li key={item.id}>
-                            {item.title}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <MuiThemeProvider theme={theme}>
+                <div className="App">
+
+                    <header className="App-header">
+                        <h1 className="App-title">
+                            Watch
+                        </h1>
+                        <h2 className="App-subtitle">
+                            Movies and TV Shows
+                        </h2>
+                    </header>
+
+                    <main>
+                        <ItemTable items={items} />
+                    </main>
+
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
---
+++ src/ItemTable.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import './ItemTable.css';
+
+const ItemTable = props => {
+    const { items } = props;
+    return (
+        <Paper>
+            <Table className="ItemTable">
+                <TableHead className="ItemTable-head">
+                    <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Type</TableCell>
+                        <TableCell className="ItemTable-skinny-col" />
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {items.map(item => (
+                        <TableRow key={item.id}>
+                            <TableCell component="th" scope="row">{item.title}</TableCell>
+                            <TableCell>{item.type}</TableCell>
+                            <TableCell className="ItemTable-skinny-col">
+                                <Button>Edit</Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+};
+
+ItemTable.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.object)
+};
+
+ItemTable.defaultProps = {
+    items: []
+};
+
+export default ItemTable;
