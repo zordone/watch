51a1716d0e4292c63e0fe4885a4416496e14cae3
Zord <zord.bk@gmail.com>
2018-08-28 17:45:12 +0200

    Added start-all script

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ An app to keep track of the movies and shows I'm interested in.
 * Start Mongo DB: `mongod`
 * Start backend: `npm run server`
 * Start frontend: `npm start`
+* Start all in one: `npm run start-all`
---
+++ scripts/start-all.js
@@ -0,0 +1,35 @@
+// - Starts the database
+// - Waits until it's ready for connections
+// - Starts the server
+// - Starts the client
+// - Opens the client in the default browser
+
+const spawn = require('child_process').spawn;
+
+const spawnPipe = (command, args = [], prefix = '', onLine = () => {}) => {
+    const db = spawn(command, args, {stdio: ['ignore', 'pipe', process.stderr]});
+    db.stdout.on('data', data => {
+        data.toString()
+            .split('\n')
+            .forEach(line => {
+                console.log(prefix, line);
+                setTimeout(() => onLine(line), 0);
+            });
+    });
+};
+
+let isAllStarted = false;
+
+spawnPipe('mongod', [], '[DB]', line => {
+    if (isAllStarted) {
+        return;
+    }
+    const isStarted = line.includes('waiting for connections on port');
+    const isAlreadyRunning = line.includes('Another mongod instance is already running');
+    if (isStarted || isAlreadyRunning) {
+        spawnPipe('npm', ['run', 'server'], '[BE]');
+        spawnPipe('npm', ['start'], '[FE]');
+        console.log('\nAll processes started.\n');
+        isAllStarted = true;
+    }
+});
