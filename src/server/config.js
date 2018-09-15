79c7e392abaaad23a13486b5bbc1209cf5b0b7e5
Zord <zord.bk@gmail.com>
2018-09-15 15:19:12 +0200

    Moved backup directory to user home

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,10 @@
+const homeDir = require('os').homedir();
+
 module.exports = Object.freeze({
     PORT: 3001,
     DATABASE_URL: 'mongodb://localhost:27017/watch',
-    IMPORT_DIR: '/Users/zord/Development/watch/_work/import',
-    BACKUP_DIR: '/Users/zord/Development/watch/_work/backup'
+    BACKUP_DIR: `${homeDir}/Documents/watch-db-backups`,
+    // this was used for the old CSV import. not needed currently.
+    // will be needed for the planned JSON restore feature.
+    IMPORT_DIR: ''
 });
